6abd7a10ad7660daf01f4341393865453799debf
santiagodiazpace <santiagodiazpace@hotmail.com>
2026-09-07 21:06:33 -0300

    Sección M-Rodriguez

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { InstitutionalComponent } from './institutional.component';
 import { NewsComponent } from './news.component';
 import { CalendarComponent } from './calendar.component';
 import { ParishComponent } from './parish.component';
+import { MartinRodriguezComponent } from './martin-rodriguez.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, title: 'Unidades Académicas' },
@@ -26,6 +27,11 @@ export const routes: Routes = [
     path: 'parroquia',
     component: ParishComponent,
     title: 'Parroquia | Unidades Académicas',
+  },
+  {
+    path: 'm-rodriguez',
+    component: MartinRodriguezComponent,
+    title: 'Colegio Martín Rodríguez | Unidades Académicas',
   },
   { path: '**', redirectTo: '' },
 ];
---
+++ src/app/martin-rodriguez.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-martin-rodriguez',
+  imports: [RouterLink],
+  templateUrl: './martin-rodriguez.component.html',
+})
+export class MartinRodriguezComponent {}
